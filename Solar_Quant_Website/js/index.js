0dded7cfc51f18a91cfd1ac78d34a902659e560d
Dineth Rajapakse <dineth.r1@outlook.com>
2025-04-08 21:16:29 +1000

    Fourth commit

--- Solar_Quant_Website/js/index.js
+++ Solar_Quant_Website/js/index.js
@@ -1,1 +1,8 @@
-console.log('Test')
+import {createRoot} from 'react-dom/client';
+
+//clear the exisiting HTML content. 
+document.body.innerHTML = '<div id="app"></div>'
+
+//Render your React component instead
+const root = createRoot(document.getElementById('app'));
+root.render(<h1>Hello World.</h1>);
