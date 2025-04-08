1398442558d2f96dac9ca6b6a76b5fff451e0761
Dineth Rajapakse <dineth.r1@outlook.com>
2025-04-08 21:14:58 +1000

    Third commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+        <h1>Test</h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae nobis earum consequatur. Aut tempora possimus odio! Distinctio perferendis delectus ratione optio aut nesciunt repellat similique, quasi libero excepturi quo a?</p>
+    </div>
+  )
+}
+
+export default App;
